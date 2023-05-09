71dc2d03da69486f11fc956089dcf0ddd3dbbee9
Emil Wibe <mail@ew.dk>
2023-05-09 12:22:18 +0200

    [feat] 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 
-    <h1>Siden du leder efter findes ikke</h1>
+    <h1>404-fejl</h1>
 
+    <div class="flexible-content">
+        <p>Det var mærkeligt. Måske har Vi forlagt den side du leder efter? Den findes ikke her</p>
+    </div>
 <?php get_footer(); ?>
